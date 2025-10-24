cde5eee8cc005c3fb6570e30020593cdad99070d
brilliant751 <brilliant751@brilliant751deMacBook-Pro.local>
2025-10-24 20:50:38 +0800

    xslx included

--- miniprogram/pages/form/form.js
+++ miniprogram/pages/form/form.js
@@ -1,11 +1,15 @@
-// pages/form.js
+const XLSX = require('../../utils/xlsx.full.min.js');
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    testrow: [
+      { 姓名: '张三', 手机: '138****0001', 反馈: '很好' },
+      { 姓名: '李四', 手机: '139****0002', 反馈: '一般' }
+    ]
   },
 
   /**
@@ -62,5 +66,42 @@ Page({
    */
   onShareAppMessage() {
 
+  },
+
+
+  onSubmit() {
+    console.log("表单已提交");
+    const fs = wx.getFileSystemManager();
+    const filepath = `${wx.env.USER_DATA_PATH}/test.xlsx`;
+    const bookdata = this.generateXLSX(this.data.testrow);
+    fs.writeFile({
+      filePath: filepath,
+      data: bookdata,
+      encoding: 'binary',
+      success: () => {
+        console.log("文件保存成功");
+        wx.openDocument({
+          filePath: filepath,
+          fileType: 'xlsx',
+          showMenu: true
+        });
+      },
+      fail: (err) => {
+        console.error("文件保存失败", err);
+      }
+    });
+  },
+
+  // xlsx生成函数
+  // 将JSON数据转换为XLSX格式
+  // 并返回二进制数据
+  generateXLSX(data) {
+    const workbook = XLSX.utils.book_new();
+    const worksheet = XLSX.utils.json_to_sheet(data);
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+    const wopts = { bookType: 'xlsx', type: 'array' };
+    const binaryData = XLSX.write(workbook, wopts);
+    return binaryData;
   }
+
 })
